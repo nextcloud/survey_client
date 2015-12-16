1cb4cc7091244ff957056269a9da6b8da6e806ae
Joas Schilling <nickvergessen@owncloud.com>
2015-12-16 13:01:25 +0100

    Add a collector that combines all the infos

---
+++ collector.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @author Joas Schilling <nickvergessen@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\PopularityContestClient;
+
+use bantu\IniGetWrapper\IniGetWrapper;
+use OCA\PopularityContestClient\Categories\Apps;
+use OCA\PopularityContestClient\Categories\Database;
+use OCA\PopularityContestClient\Categories\Encryption;
+use OCA\PopularityContestClient\Categories\Files_Sharing;
+use OCA\PopularityContestClient\Categories\ICategory;
+use OCA\PopularityContestClient\Categories\Locking;
+use OCA\PopularityContestClient\Categories\OwnCloud;
+use OCA\PopularityContestClient\Categories\php;
+use OCA\PopularityContestClient\Categories\Stats;
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\IL10N;
+
+class Collector {
+
+	/** @var ICategory[] */
+	protected $categories;
+
+	/** @var IConfig */
+	protected $config;
+	
+	/** @var IDBConnection */
+	protected $connection;
+
+	/** @var IniGetWrapper */
+	protected $phpIni;
+
+	/** @var \OCP\IL10N */
+	protected $l;
+
+	/**
+	 * Collector constructor.
+	 *
+	 * @param IConfig $config
+	 * @param IDBConnection $connection
+	 * @param IniGetWrapper $phpIni
+	 * @param IL10N $l
+	 */
+	public function __construct(IConfig $config, IDBConnection $connection, IniGetWrapper $phpIni, IL10N $l) {
+		$this->config = $config;
+		$this->connection = $connection;
+		$this->phpIni = $phpIni;
+		$this->l = $l;
+	}
+
+	protected function registerCategories() {
+		$this->categories = [];
+		$this->categories[] = new OwnCloud(
+			$this->config,
+			$this->l
+		);
+		$this->categories[] = new php(
+			$this->phpIni,
+			$this->l
+		);
+		$this->categories[] = new Database(
+			$this->config,
+			$this->connection,
+			$this->l
+		);
+		$this->categories[] = new Apps(
+			$this->connection,
+			$this->l
+		);
+		$this->categories[] = new Stats(
+			$this->connection,
+			$this->l
+		);
+		$this->categories[] = new Files_Sharing(
+			$this->connection,
+			$this->l
+		);
+		$this->categories[] = new Encryption(
+			$this->config,
+			$this->l
+		);
+		$this->categories[] = new Locking(
+			$this->config,
+			$this->l
+		);
+	}
+
+	public function getReport() {
+		$this->registerCategories();
+
+		$tuples = [];
+
+		foreach ($this->categories as $category) {
+			foreach ($category->getData() as $key => $value) {
+				$tuples[] = [
+					$category->getCategory(),
+					$key,
+					$value
+				];
+			}
+		}
+
+		return [
+			'id' => $this->config->getSystemValue('instanceid'),
+			'items' => $tuples,
+		];
+	}
+}
