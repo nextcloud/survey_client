13d93c9381e4c1ab18ecc80829e2e706ff3bcf05
Bjoern Schiessle <bjoern@schiessle.org>
2016-07-08 14:29:39 +0200

    update license headers

--- admin.php
+++ admin.php
@@ -1,14 +1,21 @@
 <?php
 /**
- * ownCloud Workflow
+ * @author Joas Schilling <coding@schilljs.com>
  *
- * @author Joas Schilling <nickvergessen@owncloud.com>
- * @copyright 2015 ownCloud, Inc.
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @license AGPL-3.0
  *
- * This code is covered by the ownCloud Commercial License.
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
  *
- * You should have received a copy of the ownCloud Commercial License
- * along with this program. If not, see <https://owncloud.com/licenses/owncloud-commercial/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
 
--- appinfo/app.php
+++ appinfo/app.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @author Joas Schilling <nickvergessen@owncloud.com>
+ * @author Joas Schilling <coding@schilljs.com>
  *
- * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
  * @license AGPL-3.0
  *
  * This code is free software: you can redistribute it and/or modify
